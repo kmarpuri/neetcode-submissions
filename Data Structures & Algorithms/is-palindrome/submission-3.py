e5ecfac23b3947d3fa90b2e074a4482caa3b1c5b
Krish Marpuri <Kmarpuri23@gmail.com>
2026-08-11 20:17:02 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = "".join(filter(str.isalnum, s)).lower()
+        l, r = 0, len(cleaned)-1
+        while l < r:
+            if cleaned[l] != cleaned[r]:
+                return False
+            l+=1
+            r-=1
+        return True
